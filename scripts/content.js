c3586bb9cd7bf77ed6e7c6f5d54090bf2e139697
Denton Liu <liu.denton+github@gmail.com>
2016-12-25 06:24:01 -0500

    Refactor content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,36 @@
 "use strict";
 
-var control_keycode = 17;
+var controlKeycode = 17;
+
+var originalTitle = null;
 
 function emitCtrlEvent(isKeyDown) {
 	return {
-		message: "ctrl_event",
+		message: "ctrlEvent",
 		isKeyDown: isKeyDown
 	};
 }
 
-window.addEventListener("keydown", function(event) {
-	if (event.keyCode === control_keycode) {
+window.addEventListener("keydown", function (event) {
+	if (event.keyCode === controlKeycode) {
 		chrome.runtime.sendMessage(emitCtrlEvent(true));
 	}
 });
 
-window.addEventListener("keyup", function(event) {
-	if (event.keyCode === control_keycode) {
+window.addEventListener("keyup", function (event) {
+	if (event.keyCode === controlKeycode) {
 		chrome.runtime.sendMessage(emitCtrlEvent(false));
+	}
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+	if (sender.id === chrome.runtime.id && message.message === "titleEvent") {
+		if (message.tabNumber === 0 && originalTitle !== null) {
+			document.title = originalTitle;
+			originalTitle = null;
+		} else if (message.tabNumber > 0 && originalTitle === null) {
+			originalTitle = document.title;
+			document.title = message.tabNumber + ". " + document.title;
+		}
 	}
 });
